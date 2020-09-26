c421e09cc577116eb4d76817316073f77c9b299a
Zachary Chua <zchua025@e.ntu.edu.sg>
2020-09-26 22:54:55 +0800

    olympiad cache

--- codeitsuisse/routes/olympiad.py
+++ codeitsuisse/routes/olympiad.py
@@ -9,19 +9,6 @@ from codeitsuisse import app;
 logger = logging.getLogger(__name__)
 
 import itertools
-def knapsack(books,time):
-    if time==0:
-        return [0,books]
-    overflow=True
-    book_arr=[]
-    for i in range(len(books)):
-        if time-books[i]>=0:
-            overflow=False
-            book_arr.append(knapsack(books[:i]+books[i+1:],time-books[i]))
-    if overflow:
-        return [time,books]
-    
-    return min(book_arr,key=lambda x:x[0])
 
 
 @app.route('/olympiad-of-babylon', methods=['POST'])
@@ -36,6 +23,24 @@ def evaluate_olympiad():
     len_bs=len(book_shelf)
     day_lists=list(itertools.permutations(days_og))
     outputs=[]
+    cache={}
+
+    def knapsack(books,time):
+        if time==0:
+            return [0,books]
+        overflow=True
+        book_arr=[]
+        for i in range(len(books)):
+            if time-books[i]>=0:
+                overflow=False
+                if (tuple(books[:i]+books[i+1:]),time-books[i]) not in cache:
+                    cache[(tuple(books[:i]+books[i+1:]),time-books[i])]=knapsack(books[:i]+books[i+1:],time-books[i])
+                book_arr.append(cache[(tuple(books[:i]+books[i+1:]),time-books[i])])
+        if overflow:
+            return [time,books]
+        
+        return min(book_arr,key=lambda x:x[0])
+
     for days in day_lists:
         res=0
         for time in days:

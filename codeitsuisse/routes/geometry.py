88e5cc40d18c17a502cbda0b3b9fe222b2272c40
Benny Koh <benny@Bennys-MacBook-Pro.local>
2020-09-26 16:36:33 +0800

    changed endpoint name

--- codeitsuisse/routes/geometry.py
+++ codeitsuisse/routes/geometry.py
@@ -7,7 +7,7 @@ from codeitsuisse import app;
 
 logger = logging.getLogger(__name__)
 
-@app.route('/geometry', methods=['POST'])
+@app.route('/revisitgeometry', methods=['POST'])
 def geometry_eval():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))

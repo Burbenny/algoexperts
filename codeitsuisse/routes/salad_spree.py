d5afd88954600e98dec6f0629364d82a2667bc79
limxuanyu127 <limxuanyu127@gmail.com>
2020-09-26 12:46:05 +0800

    salad-spree-v3

--- codeitsuisse/routes/salad_spree.py
+++ codeitsuisse/routes/salad_spree.py
@@ -20,42 +20,60 @@ def evaluateSaladSpree():
     res = float('inf')
 
     for street in streetMap:
-        streetIdx = 0
-        # 0 -2 out of 5
-        while streetIdx <= (len(street)-n):
-
-
-
-            #Check if any value in the n-window == "X"
-            moveToNextIdx = False
-            for checkX in reversed(range(n)):
-                toCheck = streetIdx + checkX
-                if toCheck > len(street)-1:
-                    continue
-                elif street[toCheck] == "X":
-                    streetIdx += checkX+1
-                    moveToNextIdx = True
-                    break
-
-            if streetIdx + n-1 > len(street)-1 or moveToNextIdx:
-                break
-
+        for streetIdx in range(len(street)- n +1):
             neighbourSum = 0
-            print("street", street)
+            updateRes = True
             for k in range(streetIdx, streetIdx + n ):
-                print("im here")
+
+                if street[k] == "X":
+                    updateRes = False
+                    break
                 neighbourSum += int(street[k])
-
-            if neighbourSum < res:
+            if updateRes and neighbourSum < res:
                 res = neighbourSum
-
-            streetIdx += 1
-
 
     if res == float('inf'):
         res = 0
 
     resDict = {"result": res}
+
+    # for street in streetMap:
+    #     streetIdx = 0
+    #     # 0 -2 out of 5
+    #     while streetIdx <= (len(street)-n):
+    #
+    #
+    #
+    #         #Check if any value in the n-window == "X"
+    #         moveToNextIdx = False
+    #         for checkX in reversed(range(n)):
+    #             toCheck = streetIdx + checkX
+    #             if toCheck > len(street)-1:
+    #                 continue
+    #             elif street[toCheck] == "X":
+    #                 streetIdx += checkX+1
+    #                 moveToNextIdx = True
+    #                 break
+    #
+    #         if streetIdx + n-1 > len(street)-1 or moveToNextIdx:
+    #             break
+    #
+    #         neighbourSum = 0
+    #         print("street", street)
+    #         for k in range(streetIdx, streetIdx + n ):
+    #             print("im here")
+    #             neighbourSum += int(street[k])
+    #
+    #         if neighbourSum < res:
+    #             res = neighbourSum
+    #
+    #         streetIdx += 1
+    #
+    #
+    # if res == float('inf'):
+    #     res = 0
+    #
+    # resDict = {"result": res}
 
     logging.info("My result :{}".format(resDict))
     return jsonify(resDict);
